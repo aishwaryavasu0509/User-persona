aee3ff9a83cc6d3799ba28bf5665a50c6aca9e2c
aishwaryavasu0509 <61535855+aishwaryavasu0509@users.noreply.github.com>
2020-10-07 20:56:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const app=express();
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
 //import routes
-const authroute = require('./routes/auth');
+const authroute = require("./routes/auth");
 
 //connect to db
-mongoose.connect('mongodb+srv://aishwaryavasukannakumar:aish@2000@cluster0.cdjiv.mongodb.net/<dbname>?retryWrites=true&w=majority',
-{useNewUrlParser: true},
-() => console.log('connected to db'));
+mongoose.connect(
+  "mongodb+srv://aishwaryavasukannakumar:aish@2000@cluster0.cdjiv.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  { useNewUrlParser: true,useUnifiedTopology: true },
+  () => console.log("connected to db")
+);
 
 //middleware
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("hello!"));
 
 //route middlewares
-app.use('/api/user',authroute);
+app.use("/api/user", authroute);
 
-
-app.listen(3000,() => console.log('server up'));
+app.listen(3000, () => console.log("server up on 3000"));
